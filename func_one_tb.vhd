058a1d28d00a091fc3cc122c77c401d54d527d4e
siim talvik <siim@talvik.eu>
2015-10-28 00:06:53 +0200

    initial structure created

---
+++ func_one_tb.vhd
@@ -0,0 +1,17 @@
+------------------------------------------------------------------------------
+--  File: func_one_tb.vhd
+------------------------------------------------------------------------------
+--Adder testbench
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+--Testbench entity is always empty
+entity funcOneTestBench is
+end funcOneTestBench;
+
+architecture Bench of funcOneTestBench is 
+	
+begin
+	
+
+end Bench;
