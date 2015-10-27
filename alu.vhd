0739f1c99686e361eeee3eb983333b8544437eee
siim talvik <siim@talvik.eu>
2015-10-28 01:40:30 +0200

    remove useless component declaration in alu.vhd

--- alu.vhd
+++ alu.vhd
@@ -19,12 +19,6 @@ component MUX is Port (
 			o : out  STD_LOGIC_VECTOR (3 downto 0)); --4 bit output 
 end component MUX;
 
-component func_one is Port (
-	a : in  STD_LOGIC_VECTOR (3 downto 0); --4 bit input
-	b : in  STD_LOGIC_VECTOR (3 downto 0); -- 4 bit input 
-	o : out  STD_LOGIC_VECTOR (3 downto 0)); --4 bit output 
-end component func_one;
-
 --declare components and inner signals here if needed
 
 signal f1, f2, f3, f4 : STD_LOGIC_VECTOR (3 downto 0);
@@ -32,7 +26,7 @@ signal f1, f2, f3, f4 : STD_LOGIC_VECTOR (3 downto 0);
 begin --beginning of the architecture
 	--f1 <= "0001";
 	--f2 <= "0010";
-	f3 <= "0100";
+	--f3 <= "0100";
 	f4 <= "1000";
 	
 	func_1 : func_one port map(
@@ -44,6 +38,11 @@ begin --beginning of the architecture
 	func_2 : func_two port map(
 		a => a,
 		o => f2
+	);
+	
+	func_3 : func_three port map(
+		a => a,
+		o => f3
 	);
 	
 	mux_1 : MUX port map(
